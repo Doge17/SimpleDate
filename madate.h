cc707a54362b4ab0e653981ebdfae114d4d14fc7
Zidani Mehdi <40828634+Doge17@users.noreply.github.com>
2019-01-03 00:38:46 +0100

    Add files via upload

---
+++ madate.h
@@ -0,0 +1,31 @@
+#pragma once
+class madate
+{
+	int _jour;
+	int _mois;
+	long int _annee;
+	 int nbrjour[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };// si le mois est trente ou non
+public:
+	madate();
+	madate(int ctorjour, int ctormois,long int ctorannee);
+	~madate();
+	int jourecoule();
+	int totalMois();
+	int TotalAnnee(madate lhs);
+	int jourecoule(madate lhs);
+	bool isBesxtile();
+	int operator-(madate lhs);
+	int operator-=(madate lhs);
+	const int enNum() ;
+	const int enTouteLettre() ;
+	void setMois(int newMois);
+	void setAnnee(int newAnnee);
+	void setDay(int newDay);
+	bool const operator>(madate &lhs);
+	void operator++();
+	long int excel();
+	bool const operator<(madate& lhs);
+	bool const operator==(madate& lhs);
+	
+};
+
