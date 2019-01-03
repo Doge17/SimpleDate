8bd370d7a79846e6135ea4e0a22fcd912452f89e
Zidani Mehdi <40828634+Doge17@users.noreply.github.com>
2019-01-03 18:57:28 +0000

    Update MaDate.cpp

--- MaDate.cpp
+++ MaDate.cpp
@@ -20,7 +20,7 @@ sakamoto(int d, int m, int y)
 {
 	int Increment[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
 	y -= m < 3;
-	return (y + y / 4 - y / 100 + y / 400 + Increment[m - 1] + d) % 7;
+	return (((y + y / 4 - y / 100 + y / 400 + Increment[m - 1] + d) %7)+7)% 7;
 }
 
 int
